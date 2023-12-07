86a2aa4ed9e92ef6968ae1517cedf09d64aaf61e
Pedro Paulo <42193455+pedropauloo@users.noreply.github.com>
2023-12-07 01:01:30 -0300

    feat: add hole obstacle

--- scenes/game/levels/level_1/level_1.gd
+++ scenes/game/levels/level_1/level_1.gd
@@ -1,17 +1,30 @@
 extends Level 
 @onready var id = 10
 @onready var wall = load("res://scenes/game/objects/wall/wall.tscn")
+@onready var hole = load("res://scenes/game/objects/hole/hole.tscn")
+
 var wall_ready = true
+var hole_ready = true
 
 func _process(delta):
 	super._process(delta)
+	
 	if(wall_ready):
 		var wall_inst = wall.instantiate()
 		wall_inst.position.x = 50 
 		wall_inst.position.z = randi_range(16,-16)
 		get_tree().get_root().add_child(wall_inst)
 		wall_ready = false
+		
+	if(hole_ready):
+		var hole_instance = hole.instantiate()
+		hole_instance.position.x = 50
+		hole_instance.position.z = randi_range(16, -16)
+		get_tree().get_root().add_child(hole_instance)
+		hole_ready = false
+		
 	
 
 func _on_timer_timeout():
 	wall_ready = true
+	hole_ready = false
---
+++ scenes/game/objects/hole/hole.gd
@@ -0,0 +1,15 @@
+extends Area3D
+@export var speed = 20
+
+func _physics_process(delta):
+	var new_position = transform.origin
+	new_position.x -= speed * delta
+	transform.origin = new_position
+
+func _process(delta):
+	if(position.x < -10):
+		queue_free()
+
+func _on_body_entered(body):
+	if (body is Player):
+		body.hp = 0
