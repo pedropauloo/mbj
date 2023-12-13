0e6e98bed5560f1b54b21314a39e8b9edb8fa638
Pedro Paulo <42193455+pedropauloo@users.noreply.github.com>
2023-12-13 18:02:40 -0300

    fix: adjust on obstacles

--- scenes/game/levels/level_1/level_1.gd
+++ scenes/game/levels/level_1/level_1.gd
@@ -1,7 +1,5 @@
 extends Level 
-
 @onready var id = 10
-
 @onready var wall = load("res://scenes/game/objects/wall/wall.tscn")
 @onready var hole = load("res://scenes/game/objects/hole/hole.tscn")
 @onready var oil = load("res://scenes/game/objects/oil/oil.tscn")
@@ -10,14 +8,17 @@ var wall_ready = true
 var hole_ready = true
 var oil_ready = true
 
+func _ready():
+	for i in $Obstacles.get_children():
+		i.queue_free()
+		
 func _process(delta):
 	super._process(delta)
-	
 	if(wall_ready):
 		var wall_inst = wall.instantiate()
 		wall_inst.position.x = 50 
 		wall_inst.position.y = 1 
-		wall_inst.position.z = randi_range(16,-16)
+		wall_inst.position.z = randi_range(2,-5)
 		$Obstacles.add_child(wall_inst)
 		wall_ready = false
 		
@@ -25,7 +26,7 @@ func _process(delta):
 		var hole_instance = hole.instantiate()
 		hole_instance.position.x = 50
 		hole_instance.position.y = 1 
-		hole_instance.position.z = randi_range(16,-16)
+		hole_instance.position.z = randi_range(2,-5)
 		$Obstacles.add_child(hole_instance)
 		hole_ready = false
 		
@@ -33,11 +34,16 @@ func _process(delta):
 		var oil_instance = oil.instantiate()
 		oil_instance.position.x = 50
 		oil_instance.position.y = 1 
-		oil_instance.position.z = randi_range(16,-16)
+		oil_instance.position.z = randi_range(2,-5)
 		$Obstacles.add_child(oil_instance)
 		oil_ready = false
-		
-func _on_timer_timeout():
+	
+	
+func _on_timer_wall_timeout():
 	wall_ready = true
 	hole_ready = true
 	oil_ready = true
+
+func _on_timer_points_timeout():
+	score += 1
+	$Score.text = str(score) + 'm'
