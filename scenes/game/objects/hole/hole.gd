9a8abffa4c5cf7bf7f21e0ff7d0c839b16e00859
Jorgelino328 <jorgewilliam328@gmail.com>
2023-12-20 22:01:53 -0300

    fix: some weird bug with the backgrounds

--- scenes/game/objects/hole/hole.gd
+++ scenes/game/objects/hole/hole.gd
@@ -5,12 +5,12 @@ func _ready():
 	super._ready()
 	match get_parent().get_parent().id:
 		10:
-			$Sprite2D.modulate = Color(0.45,0.35,0.11,1)
+			$Sprite3D.modulate = Color(0.45,0.35,0.11,1)
 		20:
-			$Sprite2D.modulate = Color(0.44,0.41,0.42,1)
+			$Sprite3D.modulate = Color(0.44,0.41,0.42,1)
 		30:
-			$Sprite2D.texture = spikes
-			$Sprite2D.modulate = Color(1,1,1,1)
+			$Sprite3D.texture = spikes
+			$Sprite3D.modulate = Color(1,1,1,1)
 
 func _on_body_entered(body):
 	if (body is Player):
@@ -22,3 +22,4 @@ func _on_body_entered(body):
 		status = 2
 		get_parent().get_parent().get_node("Police").animation_status = 2
 		set_speed_factor(speed_factor + speed_variation)
+		
